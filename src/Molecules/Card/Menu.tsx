dc955327939584fec954e7e91b43387c3f53108c
gupi debnath <iamgupidebnath@gmail.com>
2025-01-16 12:47:06 +0530

    fix login page

--- src/Molecules/Card/Menu.tsx
+++ src/Molecules/Card/Menu.tsx
@@ -22,15 +22,36 @@ const ProjectMenu = ({
       onClose={onClose}
       anchorOrigin={{
         vertical: 'top',
-        horizontal: 'left',
+        horizontal: 'right',
       }}
       transformOrigin={{
         vertical: 'top',
-        horizontal: 'right',
+        horizontal: 'left',
+      }}
+      PaperProps={{
+        style: {
+          borderRadius: 8,
+          padding: '8px 0',
+          boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+          minWidth: 150,
+          backgroundColor: '#f9f9f9',
+        },
       }}
     >
       {menuItems.map((item, index) => (
-        <MenuItem key={index} onClick={() => onOptionClick(item)}>
+        <MenuItem
+          key={index}
+          onClick={() => onOptionClick(item)}
+          sx={{
+            fontSize: '14px',
+            fontWeight: 500,
+            color: '#333',
+            padding: '10px 16px',
+            '&:hover': {
+              backgroundColor: '#e0e0e0',
+            },
+          }}
+        >
           {item}
         </MenuItem>
       ))}
