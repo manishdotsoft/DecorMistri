5fc78976a7e5c3e3fa21e37845939dd1f7cc1bfa
gupi debnath <iamgupidebnath@gmail.com>
2025-01-28 10:57:37 +0530

    Feature-data

---
+++ src/Data/CardData.tsx
@@ -0,0 +1,147 @@
+export const CompleteProjectData = [
+  {
+    id: 1,
+
+    value: 'Complete Project 1',
+    designType: 'Residential',
+    location: 'India',
+    startDate: '2025-01-01',
+    endDate: '2025-12-31',
+    completionPercentage: 75,
+    status: 'Complete',
+  },
+  {
+    id: 2,
+
+    value: 'Complete Project 2',
+    designType: 'Commercial',
+    location: 'India',
+    startDate: '2025-01-01',
+    endDate: '2025-12-31',
+    completionPercentage: 25,
+    status: 'Complete',
+  },
+  {
+    id: 3,
+
+    value: 'Complete Project 3',
+    designType: 'Commercial',
+    location: 'India',
+    startDate: '2025-01-01',
+    endDate: '2025-12-31',
+    completionPercentage: 25,
+    status: 'Complete',
+  },
+];
+export const NotConfirmedProjectData = [
+  {
+    id: 4,
+
+    value: 'Not Confirmed',
+    designType: 'Mixed-Use',
+    location: 'India',
+    startDate: 'TBD',
+    endDate: 'TBD',
+    completionPercentage: 0,
+    status: 'NotConfirmed',
+  },
+  {
+    id: 5,
+
+    value: 'Not Confirmed',
+    designType: 'Mixed-Use',
+    location: 'India',
+    startDate: 'TBD',
+    endDate: 'TBD',
+    completionPercentage: 0,
+    status: 'NotConfirmed',
+  },
+  {
+    id: 6,
+
+    value: 'Not Confirmed',
+    designType: 'Mixed-Use',
+    location: 'India',
+    startDate: 'TBD',
+    endDate: 'TBD',
+    completionPercentage: 0,
+    status: 'NotConfirmed',
+  },
+];
+
+export const UpcomingProjectData = [
+  {
+    id: 7,
+    value: 'Upcoming Project 1',
+    designType: 'Residential',
+    location: 'India',
+    startDate: '2025-01-01',
+    endDate: '2025-12-31',
+    completionPercentage: 75,
+    status: 'Upcoming',
+  },
+  {
+    id: 8,
+
+    value: 'Upcoming Project 2',
+    designType: 'Commercial',
+    location: 'India',
+    startDate: '2025-01-01',
+    endDate: '2025-12-31',
+    completionPercentage: 25,
+    status: 'Upcoming',
+  },
+  {
+    id: 9,
+
+    value: 'Upcoming Project 3',
+    designType: 'Commercial',
+    location: 'India',
+    startDate: '2025-01-01',
+    endDate: '2025-12-31',
+    completionPercentage: 25,
+    status: 'Upcoming',
+  },
+];
+export const LiveProjectData = [
+  {
+    id: 10,
+
+    value: 'Live Project 1',
+    designType: 'Residential',
+    location: 'India',
+    startDate: '2025-01-01',
+    endDate: '2025-12-31',
+    completionPercentage: 75,
+    status: 'Live',
+  },
+  {
+    id: 11,
+
+    value: 'Live Project 2',
+    designType: 'Commercial',
+    location: 'India',
+    startDate: '2025-01-01',
+    endDate: '2025-12-31',
+    completionPercentage: 25,
+    status: 'Live',
+  },
+  {
+    id: 12,
+
+    value: 'Live Project 3',
+    designType: 'Commercial',
+    location: 'India',
+    startDate: '2025-01-01',
+    endDate: '2025-12-31',
+    completionPercentage: 25,
+    status: 'Live',
+  },
+];
+export const menuItems = [
+  'Edit',
+  'Change Status',
+  'Add Phase',
+  'Add Task',
+  'Delete Project',
+];
